314dafd3a317abd014331f065b4e8e98dd0b0a33
Elezthem <97851451+Elezthem@users.noreply.github.com>
2023-07-15 22:10:35 +0300

    Add files via upload

---
+++ CSLight/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] petrovich = new int[3];
+            int[] ivanovich = new int[5];
+            int[] sidorovich;
+
+            petrovich[0] = 5;
+            petrovich[1] = 3;
+
+            sidorovich = petrovich;
+
+            sidorovich[2] = 7;
+            sidorovich[0] = 10;
+
+            Console.WriteLine(ivanovich[0]);
+            Console.WriteLine(ivanovich[1]);
+
+            petrovich = new int[10];
+
+        }
+    }
+}
